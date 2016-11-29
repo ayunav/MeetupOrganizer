ed308856b7b5f4584621fc06516aa399589fe957
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-11-29 14:29:51 -0500

    Initial Commit

---
+++ MeetupOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetupOrganizer
+//
+//  Created by Ayuna NYC on 11/29/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
