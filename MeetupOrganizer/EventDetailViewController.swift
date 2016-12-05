cc413bb57c344333518d924ce5738de7ca1ee3d2
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-12-05 14:42:54 -0500

    Set up basic folder structure and UI layout in storyboard

---
+++ MeetupOrganizer/EventDetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  EventDetailViewController.swift
+//  MeetupOrganizer
+//
+//  Created by Ayuna NYC on 11/29/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Actions 
+    
+    @IBAction func uploadPhotosButtonTapped(_ sender: UIButton)
+    {
+        // start uiimagepicker view controller
+        // pick photos to upload 
+        // get photo multi part data
+        // construct and start url request to post photos to meetup server, pass photo data to the request 
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
