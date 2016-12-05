dfbfee2896b08c608ded1a81953647d341c09c06
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-12-05 14:43:27 -0500

    Set up basic model and networking structure

---
+++ MeetupOrganizer/Meetup.swift
@@ -0,0 +1,19 @@
+//
+//  Meetup.swift
+//  MeetupOrganizer
+//
+//  Created by Ayuna NYC on 11/29/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
+
+class Meetup
+{
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
