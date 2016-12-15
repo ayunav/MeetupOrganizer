3bb1276220dd756a9a3b98ffb634517d25d9307e
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-12-15 10:08:07 -0500

    Got the custom activity indicator view working but it's very ugly & need to refactor a bunch, but it's working, yay

--- MeetupOrganizer/EventDetailViewController.swift
+++ MeetupOrganizer/EventDetailViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import BSImagePicker
 import Photos
+import NVActivityIndicatorView
+
 
 
 // display placeholder image in the gallery collection view? 
 
 class EventDetailViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var addPhotosButton: UIButton!
     
     @IBOutlet weak var photoGalleryCollectionView: UICollectionView!
     let photoGalleryDataSource = PhotoGalleryDataSource()
@@ -24,15 +28,41 @@ class EventDetailViewController: UIViewController, UICollectionViewDelegate {
 
     var meetupAPIManager = MeetupAPI()
     
+    let client = Client()
     
-//    var photos: [UIImage] = []
-    //let meetup = Meetup(name: "iOSoho")
+    var activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0.0, y: 0.0, width: 0.0, height: 0.0))
+    
+    
+//    private func addActivityIndicatorView() {
+//        let cols = 4
+//        let cellWidth = Int(self.view.frame.width / CGFloat(cols))
+//        let xCenter = Int(view.frame.width / 2)
+//        let yCenter = Int(view.frame.height / 2)
+//        let frame = CGRect(x: xCenter, y: yCenter, width: cellWidth, height: cellWidth)
+//        //        let meetupRedColor = // add later TODO
+//        
+//        self.activityIndicatorView = NVActivityIndicatorView(frame: frame, type: .ballGridPulse, color: UIColor.red, padding: 0)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        let cols = 4
+//        let cellWidth = Int(view.frame.width / CGFloat(cols))
+//        let xCenter = Int(view.frame.width / 2)
+//        let yCenter = Int(view.frame.height / 2)
+//        let frame = CGRect(x: xCenter, y: yCenter, width: cellWidth, height: cellWidth)
+//        //        let meetupRedColor = // add later TODO
+//        
+//        self.activityIndicatorView = NVActivityIndicatorView(frame: frame, type: .ballGridPulse, color: UIColor.red, padding: 0)
+//        
+//        super.init(coder: aDecoder)
+//    }
+    
+    
     
     
     // MARK: - View Lifecycle
     
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         photoGalleryCollectionView.dataSource = photoGalleryDataSource
@@ -56,47 +86,101 @@ class EventDetailViewController: UIViewController, UICollectionViewDelegate {
             // Do something, cancel upload?
         }, cancel: { (assets: [PHAsset]) -> Void in
             // User cancelled. And this where the assets currently selected.
-        }, finish: { (assets: [PHAsset]) -> Void in
+        }, finish:
+        { (assets: [PHAsset]) -> Void in
+
+            OperationQueue.main.addOperation {
+            let cols = 4
+            let cellWidth = Int(self.view.frame.width / CGFloat(cols))
+            let xCenter = Int(self.view.frame.width / 2)
+            let yCenter = Int(self.view.frame.height / 2)
+            let frame = CGRect(x: xCenter, y: yCenter, width: cellWidth, height: cellWidth)
+            //        let meetupRedColor = // add later TODO
             
-            // 1. the col view will display selected photos, 
+            self.activityIndicatorView = NVActivityIndicatorView(frame: frame, type: .ballGridPulse, color: UIColor.red, padding: 0)
+            self.view.addSubview(self.activityIndicatorView)
+            self.activityIndicatorView.startAnimating()
+            }
+            //self.view.addActivityIndicatorToView(view: self.view)
+            
+//           self.view.addSubview(self.activityIndicatorView)
+//           self.activityIndicatorView.startAnimating()
+            
+            // 1. the col view will display selected photos,
             // change cells' alpha to opaque,
             // add spinning activity indicators indicating upload progress
             
             // 2. if there's a problem with upload, display a user facing error message
             // 3. when upload is complete & successful (json response came back), stop & hide activity indicators, change cells' aipha to clear
             
+            
             self.requestOptions.isSynchronous = true
+   
             for asset in assets {
                 self.imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.default, options: self.requestOptions, resultHandler: { (image, properties) in
-                    
+                        
                     guard let _image = image else { return }
-                    self.meetupAPIManager.uploadImageData(image: _image, groupName: "iOSoho", eventID: "232809656")
-                    // 45 sec request duration, very long
+                    
+                    self.client.uploadImageData(image: _image, groupName: "iOSoho", eventID: "233132048", completion: { (photosResult) in
+                        OperationQueue.main.addOperation {
+                            
+                        switch photosResult {
+                        case .Success:
+                            self.activityIndicatorView.stopAnimating()
+                        case let .Failure(error):
+                            print("Error fetching recent photos: \(error)")
+                        }
+                        }
+                    })
                 })
             }
-            
-            
+           
             self.photoGalleryDataSource.assets = assets
             
-            // display spinners indicating progress/success in the gallery colView cells
-            
-            OperationQueue.main.addOperation
-            {
-                self.photoGalleryCollectionView.reloadSections(NSIndexSet(index: 0) as IndexSet)
+            OperationQueue.main.addOperation {
+                    self.photoGalleryCollectionView.reloadSections(NSIndexSet(index: 0) as IndexSet)
             }
         }, completion: nil)
     }
 
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+//protocol PreparedUploadDelegate {
+//    func preparedUpload(preparedUpload: PreparedUpload, didStartLoadingCellAtIndexPath: IndexPath)
+//    func preparedUpload(preparedUpload: PreparedUpload, didFinishLoadingCellAtIndexPath: IndexPath)
+//    // TODO error handling delegate method
+//}
+//
+//struct PreparedUpload {
+//    func imageAtIndexPath(indexPath: IndexPath) -> UIImage? {
+//        return nil // TODO
+//    }
+//    
+//    var delegate: PreparedUploadDelegate?
+//    
+//    let assets: [PHAsset]
+//    init(assets: [PHAsset]) {
+//        self.assets = assets
+//        for (i, asset) in assets.enumerated() {
+//            self.delegate?.preparedUpload(preparedUpload: self, didStartLoadingCellAtIndexPath: IndexPath(row: i, section: 0)
+//            self.imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.default, options: self.requestOptions, resultHandler: { (image, properties) in
+//                
+//                guard let _image = image else { return }
+//                // 45 sec request duration, very long
+//                self.meetupAPIManager.uploadImageData(image: _image, groupName: "iOSoho", eventID: "232809656") { (photosResult) -> Void in // need a closure here, move collection view code in here, based on the result of the closure
+//                    
+//                    
+//                    OperationQueue.main.addOperation {
+//                        switch photosResult {
+//                        case let .Success(photos):
+//                            self.photoDataSource.photos = photos
+//                            self.delegate?.preparedUpload(preparedUpload: self, didFinishLoadingCellAtIndexPath: IndexPath(row: i, section: 0)
+//                            // inside the completion
+//                            // spinning indicator - stop animating if success
+//                            
+//                        }
+//                    }
+//                }
+//    }
+//}
