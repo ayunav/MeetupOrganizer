7772602cc83edadb23ad2ada50e3809be7b184e2
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-12-20 17:53:55 -0500

    Add loginVC

---
+++ MeetupOrganizer/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  MeetupOrganizer
+//
+//  Created by Ayuna NYC on 12/19/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
