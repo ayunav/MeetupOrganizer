a39c433b3753b14994f7c769e7c8e45fbe6cc4fd
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2017-01-03 17:31:47 -0500

    Add events tableview programmatically

--- MeetupOrganizer/EventsTableViewController.swift
+++ MeetupOrganizer/EventsTableViewController.swift
@@ -16,7 +16,7 @@
 
 import UIKit
 
-class EventsTableViewController: UITableViewController {
+class EventsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     private let CellIdentifier = "EventsTableViewCellIdentifier"
 
@@ -24,7 +24,7 @@ class EventsTableViewController: UITableViewController {
     
     var meetupAPI = MeetupAPI()
     
-    
+    var eventsTableView: UITableView = UITableView()
     
     override func loadView() {
         super.loadView()
@@ -33,20 +33,27 @@ class EventsTableViewController: UITableViewController {
         let items = ["Upcoming", "Past"]
         let segmentedControl = UISegmentedControl(items: items)
         segmentedControl.selectedSegmentIndex = 0
-        
-        // set the frame 
+
+        // set the segmented control frame
         let frame = UIScreen.main.bounds
         // 10% of the height of the screen
-        segmentedControl.frame = CGRect(x: frame.minX + 10, y: frame.minY + 50, width: frame.width - 50, height: frame.height * 0.1)
-        
+        segmentedControl.frame = CGRect(x: frame.minX + 10, y: (self.navigationController?.navigationBar.frame.height)! + 10, width: frame.width - 50, height: frame.height * 0.05)
+
         // style segmented control (optional)
-            // add later
+        // add later
+        segmentedControl.layer.cornerRadius = 5.0  // Don't let background bleed
+        segmentedControl.backgroundColor = UIColor.white
+        segmentedControl.tintColor = MeetupRedColor
         
-        
-        // add target action method 
+        // add target action method
         segmentedControl.addTarget(self, action: Selector(("fetchEvents:")), for: .valueChanged)
         
         self.view.addSubview(segmentedControl)
+
+        // set events table view fram
+        eventsTableView.frame = CGRect(x: frame.minX + 10, y: segmentedControl.frame.minY + segmentedControl.frame.height + 10, width: frame.width, height: frame.height - segmentedControl.frame.height - 20)
+
+        self.view.addSubview(eventsTableView)
     }
     
 
@@ -54,6 +61,12 @@ class EventsTableViewController: UITableViewController {
     {
         super.viewDidLoad()
         
+        self.navigationItem.title = "My Events"
+        
+        eventsTableView.delegate = self
+        eventsTableView.dataSource = self
+        
+        eventsTableView.register(EventsTableViewCell.self, forCellReuseIdentifier: CellIdentifier)
     }
 
     
@@ -87,7 +100,7 @@ class EventsTableViewController: UITableViewController {
                 switch eventsResult {
                 case let .Success(_events):
                     self.events = _events
-                    self.tableView.reloadData()
+                    self.eventsTableView.reloadData()
                 case let .Failure(error):
                     print("Error fetching meetup events: \(error)")
                 }
@@ -98,21 +111,21 @@ class EventsTableViewController: UITableViewController {
     
     func getPastEvents()
     {
-        
+        print("\nPast Events segmented control was tapped!!!!!!")
     }
     
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return events.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier, for: indexPath) as! EventsTableViewCell
 
         let event = events[indexPath.row]
@@ -126,18 +139,32 @@ class EventsTableViewController: UITableViewController {
     
     // MARK: - Navigation
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "ShowEventDetailVCSegueIdentifier" {
-        
-            let eventDetailVC = segue.destination as! EventDetailViewController
-            
-            if let selectedIndexPath = tableView.indexPathForSelectedRow?.row {
-            
-                let event = self.events[selectedIndexPath]
-                eventDetailVC.event = event
-            }
-        }
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//        if segue.identifier == "ShowEventDetailVCSegueIdentifier" {
+//        
+//            let eventDetailVC = segue.destination as! AddPhotosViewController
+//            
+//            if let selectedIndexPath = eventsTableView.indexPathForSelectedRow?.row {
+//            
+//                let event = self.events[selectedIndexPath]
+//                eventDetailVC.event = event
+//            }
+//        }
+//    }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let eventDetailVC = self.storyboard?.instantiateViewController(withIdentifier: "AddPhotosViewController")
+            as? AddPhotosViewController
+        
+        if let selectedIndexPath = eventsTableView.indexPathForSelectedRow?.row {
+            
+            let event = self.events[selectedIndexPath]
+            eventDetailVC?.event = event
+        }
+       
+        self.navigationController?.pushViewController(eventDetailVC!, animated: true)
+    }
 }
