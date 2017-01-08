4adc316eaa2ed12961c57877addf19a830d2db28
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-07 19:47:00 -0500

    Add ColorConstants for MeetupRedColor constant

---
+++ MeetupOrganizer/ColorConstants.swift
@@ -0,0 +1,13 @@
+//
+//  ColorConstants.swift
+//  MeetupOrganizer
+//
+//  Created by Ayuna NYC on 1/7/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+
+// HEX# ed1c40 http://uicolor.xyz/ this is amazing!
+let MeetupRedColor = UIColor(red:0.93, green:0.11, blue:0.25, alpha:1.0)
