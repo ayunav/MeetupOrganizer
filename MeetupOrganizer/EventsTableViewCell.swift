614fe51a910e7acedcaee78da7af0e664a92f5f4
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-08 12:50:53 -0500

    remove selected state (gray) for events table view cell

--- MeetupOrganizer/EventsTableViewCell.swift
+++ MeetupOrganizer/EventsTableViewCell.swift
@@ -11,23 +11,15 @@ import UIKit
 class EventsTableViewCell: UITableViewCell
 {
     @IBOutlet weak var eventDateLabel: UILabel!
-    @IBOutlet weak var groupNameLabel: UILabel!
-    @IBOutlet weak var eventTitleLabel: UILabel!
     @IBOutlet weak var eventTimeLabel: UILabel!
+    @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var groupNameLabel: UILabel!
     @IBOutlet weak var venueLabel: UILabel!
     
     
     override func awakeFromNib()
     {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool)
-    {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
