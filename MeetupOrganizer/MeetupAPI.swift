0713758ef0b73005c391775851b3f6a0a198bf85
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-08 14:38:14 -0500

    Delete comments in MeetupAPI

--- MeetupOrganizer/MeetupAPI.swift
+++ MeetupOrganizer/MeetupAPI.swift
@@ -25,35 +25,33 @@ enum PhotosResult
 
 struct MeetupAPI {
     
-    // make the API Call with the URL from API Manager class 
-    // let api manager validate json repsonse, get array of objects from the API manager
-    // pass to tvc to display in completion closure 
-
     var meetupRouter = MeetupRouter()
     
     
-    mutating func uploadImageData(image: UIImage, groupName: String, eventID: String, completion: @escaping (PhotosResult) -> Void)
-    {
-        let imageData = UIImageJPEGRepresentation(image, 0.9)
+    mutating func uploadImageData(image: UIImage, groupName: String, eventId: String, completion: @escaping (PhotosResult) -> Void) {
+     
+        let url = meetupRouter.uploadPhotosURLWithComponents(groupName: groupName, eventId: eventId)
         
-        let url = meetupRouter.uploadPhotosURLWithComponents(groupName: groupName, eventID: eventID)
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
         
-        Alamofire.upload(
-            multipartFormData: { multipartFormData in
-                multipartFormData.append(imageData!, withName: "photo", fileName: "photo.jpeg", mimeType: "image/jpeg")
-        },
-            to: url,
-            encodingCompletion: { encodingResult in
-                switch encodingResult {
-                case .success(let upload, _, _):
-                    upload.responseJSON { response in
-                        // passing completion to EventDetailVC
-                        completion(.success())
-                        debugPrint(response)
-                    }
-                case .failure(let encodingError):
-                    print(encodingError)
-                }
+        
+        Alamofire.upload(multipartFormData: { multipartFormData in
+           
+            multipartFormData.append(imageData!, withName: "photo", fileName: "photo.jpeg", mimeType: "image/jpeg")},
+
+                         to: url,
+                         
+                         encodingCompletion: { encodingResult in
+                            
+                            switch encodingResult {
+                                
+                            case .success(let upload, _, _):
+                                upload.responseJSON { response in
+                                    completion(.success())
+                                }
+                            case .failure(let encodingError):
+                                print(encodingError)
+                            }
         })
     }
     
@@ -67,25 +65,12 @@ struct MeetupAPI {
             
             guard let validResponse = response.result.value as? [[String : AnyObject]] else { return }
 
-            
-            // pass EventsResult enum .Success
-            
-            //            let result = self.eventsFromJSON(eventsJSON: validResponse)
             let events = validResponse.flatMap(Event.eventFromJsonDict)
-            //
-            //        var events = [Event]()
-            //
-            //        for eventDict in eventsJSON {
-            //            if let event = (json: eventDict) {
-            //                events.append(event)
-            //            }
-            //
-            //        }
-            //            return
+            
             if (events.count > 0) {
                 completion(EventsResult.success(events))
             } else {
-                //                completion(EventsResult.Failure(nil))
+//                completion(EventsResult.Failure(nil))
             }
         })
         
@@ -105,27 +90,10 @@ struct MeetupAPI {
             if (events.count > 0) {
                 completion(EventsResult.success(events))
             } else {
-                //                completion(EventsResult.Failure(nil))
+//                completion(EventsResult.Failure(nil))
             }
         })
         
     }
-
     
-    func eventsFromJSON(eventsJSON: [[String : AnyObject]]) -> EventsResult {
-        
-        let events = eventsJSON.flatMap(Event.eventFromJsonDict)
-        //
-        //        var events = [Event]()
-        //
-        //        for eventDict in eventsJSON {
-        //            if let event = (json: eventDict) {
-        //                events.append(event)
-        //            }
-        //
-        //        }
-        return EventsResult.success(events)
-    }
-
-
 }
